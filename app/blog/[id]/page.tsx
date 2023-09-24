5bddf59bec2c39b3c5c586d2180b0453160e8655
Almas <almas1610@gmail.com>
2023-09-24 09:59:45 +0600

    Server Component, fetch, dynamic pages: loading, error

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,17 @@
 import { Metadata } from "next";
 
+async function getDataById(id: string) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  return await response.json();
+}
+
 type Props = {
   params: {
     id: string;
@@ -9,11 +21,19 @@ type Props = {
 export async function generateMetadata({
   params: { id },
 }: Props): Promise<Metadata> {
+  const postById = await getDataById(id);
   return {
-    title: id,
+    title: postById.title,
   };
 }
 
-export default function Post({ params: { id } }: Props) {
-  return <h1>Post Page {id}</h1>;
+export default async function Post({ params: { id } }: Props) {
+  const postById = await getDataById(id);
+
+  return (
+    <>
+      <h2>{postById.title}</h2>
+      <p>{postById.body}</p>
+    </>
+  );
 }
